895e31241568cbec54acb258e568fe186a88c5a8
beastpredece <xjs5065@psu.edu>
2020-09-04 18:25:08 +0000

    space

--- main.py
+++ main.py
@@ -4,105 +4,105 @@ credit1 = input("Enter your course 1 credit: ")
 
 if grade1 == "A":
   gradepoint1 = 4.0
-  print ("Grade point for course 1 is: 4.0 ")
+  print ("Grade point for course 1 is: 4.0")
 elif grade1 == "A-":
   gradepoint1 = 3.67
-  print ("Grade point for course 1 is: 3.67 ")
+  print ("Grade point for course 1 is: 3.67")
 elif grade1 == "B+":
   gradepoint1 = 3.33
-  print ("Grade point for course 1 is: 3.33 ")
+  print ("Grade point for course 1 is: 3.33")
 elif grade1 == "B":
   gradepoint1 = 3.0
-  print ("Grade point for course 1 is: 3.0 ")
+  print ("Grade point for course 1 is: 3.0")
 elif grade1 == "B-":
   gradepoint1 = 2.67
-  print ("Grade point for course 1 is: 2.67 ")
+  print ("Grade point for course 1 is: 2.67")
 elif grade1 == "C+":
   gradepoint1 = 2.33
-  print ("Grade point for course 1 is: 2.33 ")
+  print ("Grade point for course 1 is: 2.33")
 elif grade1 == "C":
   gradepoint1 = 2
-  print ("Grade point for course 1 is: 2.0 ")
+  print ("Grade point for course 1 is: 2.0")
 elif grade1 == "D":
   gradepoint1 = 1
-  print ("Grade point for course 1 is: 1.0 ")
+  print ("Grade point for course 1 is: 1.0")
 elif grade1 == "F":
   gradepoint1 = 0
-  print ("Grade point for course 1 is: 0.0 ")
+  print ("Grade point for course 1 is: 0.0")
 
 else:
   gradepoint1 = 0
-  print ("Grade point for course 1 is: 0.0 ")
+  print ("Grade point for course 1 is: 0.0")
 
 grade2 = input("Enter your course 2 letter grade: ")
 credit2 = input("Enter your course 2 credit: ")
 
 if grade2 == "A":
   gradepoint2 = 4.0
-  print ("Grade point for course 2 is: 4.0 ")
+  print ("Grade point for course 2 is: 4.0")
 elif grade2 == "A-":
   gradepoint2 = 3.67
-  print ("Grade point for course 2 is: 3.67 ")
+  print ("Grade point for course 2 is: 3.67")
 elif grade2 == "B+":
   gradepoint2 = 3.33
-  print ("Grade point for course 2 is: 3.33 ")
+  print ("Grade point for course 2 is: 3.33")
 elif grade2 == "B":
   gradepoint2 = 3.0
-  print ("Grade point for course 2 is: 3.0 ")
+  print ("Grade point for course 2 is: 3.0")
 elif grade2 == "B-":
   gradepoint2 = 2.67
-  print ("Grade point for course 2 is: 2.67 ")
+  print ("Grade point for course 2 is: 2.67")
 elif grade2 == "C+":
   gradepoint2 = 2.33
-  print ("Grade point for course 2 is: 2.33 ")
+  print ("Grade point for course 2 is: 2.33")
 elif grade2 == "C":
   gradepoint2 = 2
-  print ("Grade point for course 2 is: 2.0 ")
+  print ("Grade point for course 2 is: 2.0")
 elif grade2 == "D":
   gradepoint2 = 1
-  print ("Grade point for course 2 is: 1.0 ")
+  print ("Grade point for course 2 is: 1.0")
 elif grade2 == "F":
   gradepoint2 = 0
-  print ("Grade point for course 2 is: 0.0 ")
+  print ("Grade point for course 2 is: 0.0")
 
 else:
   gradepoint2 = 0
-  print ("Grade point for course 2 is: 0.0 ")
+  print ("Grade point for course 2 is: 0.0")
 
 grade3 = input("Enter your course 3 letter grade: ")
 credit3 = input("Enter your course 3 credit: ")
 
 if grade3 == "A":
   gradepoint3 = 4.0
-  print ("Grade point for course 3 is: 4.0 ")
+  print ("Grade point for course 3 is: 4.0")
 elif grade3 == "A-":
   gradepoint3 = 3.67
-  print ("Grade point for course 3 is: 3.67 ")
+  print ("Grade point for course 3 is: 3.67")
 elif grade3 == "B+":
   gradepoint3 = 3.33
-  print ("Grade point for course 3 is: 3.33 ")
+  print ("Grade point for course 3 is: 3.33")
 elif grade3 == "B":
   gradepoint3 = 3.0
-  print ("Grade point for course 3 is: 3.0 ")
+  print ("Grade point for course 3 is: 3.0")
 elif grade3 == "B-":
   gradepoint3 = 2.67
-  print ("Grade point for course 3 is: 2.67 ")
+  print ("Grade point for course 3 is: 2.67")
 elif grade3 == "C+":
   gradepoint3 = 2.33
-  print ("Grade point for course 3 is: 2.33 ")
+  print ("Grade point for course 3 is: 2.33")
 elif grade3 == "C":
   gradepoint3 = 2
-  print ("Grade point for course 3 is: 2.0 ")
+  print ("Grade point for course 3 is: 2.0")
 elif grade3 == "D":
   gradepoint3 = 1
-  print ("Grade point for course 3 is: 1.0 ")
+  print ("Grade point for course 3 is: 1.0")
 elif grade3 == "F":
   gradepoint3 = 0
-  print ("Grade point for course 3 is: 0.0 ")
+  print ("Grade point for course 3 is: 0.0")
 
 else:
   gradepoint3 = 0
-  print ("Grade point for course 3 is: 0.0 ")
+  print ("Grade point for course 3 is: 0.0")
 
 credit1 = float(credit1)
 credit2 = float(credit2)
